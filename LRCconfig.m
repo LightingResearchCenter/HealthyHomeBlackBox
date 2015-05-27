053a5af088a6125d1fb11214f61f5e44e18742da
Geoffrey Jones <jonesg5@rpi.edu>
2015-05-27 16:22:33 -0400

    Created LRCconfig template

---
+++ LRCconfig.m
@@ -0,0 +1,27 @@
+function LRCconfig
+%LRCCONFIG Gloabally declare and define parameters.
+%   Define static parameters and make them accessable from a global
+%   variable.
+%
+%   % Example:
+%   %   Call LRCCONFIG to initialize the parameters. Add LRCparam to the
+%   %   current worspace. Access parameter within LRCparam.
+%
+%   LRCconfig;
+%   global LRCparam
+%   paramValue = LRCparam.paramName;
+%
+%   See also GLOBAL, STRUCT
+
+%   Author(s): G. Jones,    2015-05-27
+%   Copyright 2015 Rensselaer Polytechnic Institute. All rights reserved.
+
+global LRCparam
+
+LRCparam = struct(      ...
+    'paramName1',  1,	...
+    'paramName2',  2    ...
+    );
+
+end
+
