80b5903500142157ecd89a17dc946e156f990823
Geoffrey Jones <jonesg5@rpi.edu>
2015-05-28 17:33:29 -0400

    Updated test script for file IO functions

--- test.m
+++ test.m
@@ -1,3 +1,15 @@
+close all
+clear
+clc
+
 lightReadingPath = ['testFiles',filesep,'lightReading.csv'];
-lightReadingFid = fopen(lightReadingPath);
-[timeUTC,timeOffset,red,green,blue,cla,cs] = LRCread_lightReading(lightReadingFid);
+lightReadingFid  = fopen(lightReadingPath,'r');
+lightReading     = LRCread_lightReading(lightReadingFid);
+
+activityReadingPath = ['testFiles',filesep,'activityReading.csv'];
+activityReadingFid  = fopen(activityReadingPath,'r');
+activityReading     = LRCread_activityReading(activityReadingFid);
+
+pacemakerPath = ['testFiles',filesep,'pacemaker.csv'];
+pacemakerFid  = fopen(pacemakerPath,'r');
+pacemaker     = LRCread_pacemaker(pacemakerFid);
