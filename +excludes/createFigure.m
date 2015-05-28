2dd197a4c72df9a74a8b672527591153466d7513
Geoffrey Jones <jonesg5@rpi.edu>
2015-05-28 15:01:24 -0400

    Added backwards compatibility for plotting

--- +excludes/createFigure.m
+++ +excludes/createFigure.m
@@ -2,38 +2,53 @@ function [figureHandle,axisHandle1,axisHandle2] = createFigure
 %CREATEFIGURE Summary of this function goes here
 %   Detailed explanation goes here
 
+matlabVersionStr = version;
+shortVersionStr = regexprep(matlabVersionStr,'(\d*\.\d*)\..*','$1');
+versionNum = str2double(shortVersionStr);
+
+
 % Import supporting functions
 import excludes.*
 
 figureHandle = figure;
-configureFigure(figureHandle);
+configureFigure(figureHandle,versionNum);
 
 axisHandle1	= subplot(1,2,1);
-configureAxis1(axisHandle1)
+configureAxis1(axisHandle1,versionNum)
 
 axisHandle2	= subplot(1,2,1);
 
 % Create stop button
-createStopButton(figureHandle);
+createStopButton(figureHandle,versionNum);
 
 end
 
 
-function configureFigure(figureHandle)
+function configureFigure(figureHandle,versionNum)
 %CONFIGUREFIGURE Intialize the figure settings
 
 screenSize = get(groot,'ScreenSize');
 figureSize = [200, 200, screenSize(3)-400, screenSize(4)-400]; % left, bottom, width, height
 
-figureUnits = figureHandle.Units;
+if versionNum < 8.4 % Code for older MATLAB
+    figureUnits = get(figureHandle,'Units');
+    
+    set(figureHandle,'ToolBar','none');
+    set(figureHandle,'Units','pixels');
+    set(figureHandle,'Position',figureSize);
+    set(figureHandle,'Units',figureUnits);
+else % Code for newer MATLAB
+    figureUnits = figureHandle.Units;
 
-figureHandle.ToolBar	= 'none';
-figureHandle.Units      = 'pixels';
-figureHandle.Position	= figureSize;
-figureHandle.Units      = figureUnits;
+    figureHandle.ToolBar	= 'none';
+    figureHandle.Units      = 'pixels';
+    figureHandle.Position	= figureSize;
+    figureHandle.Units      = figureUnits;
 end
 
-function configureAxis1(axisHandle)
+end
+
+function configureAxis1(axisHandle,versionNum)
 %CONFIGUREAXIS1 Initialize the first axis
 
 hold(axisHandle,'on');
@@ -45,9 +60,6 @@ time = nowDatenum:samplingRate:nowDatenum+10;
 CS = zeros(size(time));
 AI = zeros(size(time));
 
-axisHandle.YLim = [0 1];
-axisHandle.XLim = [time(1),time(end)];
-
 plotHandle = plot(axisHandle,time,CS,'b-',time,AI,'r-');
 
 datetick('x','mm/dd','keeplimits','keepticks')
@@ -58,10 +70,24 @@ title('Simulated Input');
 
 hold(axisHandle,'off');
 
-% Link data
-plotHandle(1).XDataSource = 'timeCS';
-plotHandle(1).YDataSource = 'CS';
-plotHandle(2).XDataSource = 'timeAI';
-plotHandle(2).YDataSource = 'AI';
+
+if versionNum < 8.4 % Code for older MATLAB
+    set(axisHandle,'YLim',[0 1]);
+    set(axisHandle,'XLim',[time(1),time(end)]);
+    
+    set(plotHandle(1),'XDataSource','timeCS');
+    set(plotHandle(1),'YDataSource','CS');
+    set(plotHandle(1),'XDataSource','timeAI');
+    set(plotHandle(1),'YDataSource','AI');
+else % Code for newer MATLAB
+    axisHandle.YLim = [0 1];
+    axisHandle.XLim = [time(1),time(end)];
+
+    % Link data
+    plotHandle(1).XDataSource = 'timeCS';
+    plotHandle(1).YDataSource = 'CS';
+    plotHandle(2).XDataSource = 'timeAI';
+    plotHandle(2).YDataSource = 'AI';
+end
 
 end
--- +excludes/createStopButton.m
+++ +excludes/createStopButton.m
@@ -1,41 +1,78 @@
-function buttonHandle = createStopButton(figureHandle)
+function buttonHandle = createStopButton(figureHandle,versionNum)
 %CREATESTOPBUTTON Summary of this function goes here
 %   Detailed explanation goes here
 
-figureUnits = figureHandle.Units;
-figureHandle.Units = 'pixels';
+if versionNum < 8.4 % Code for older MALTAB
+    figureUnits = get(figureHandle,'Units');
+    set(figureHandle,Units,'pixels');
 
-buttonHandle = uicontrol(figureHandle);
-buttonHandle.Style              = 'pushbutton';
-buttonHandle.String             = 'STOP';
-buttonHandle.Position           = [20 20 100 40];
-buttonHandle.Callback           = 'runflag=false;';
-buttonHandle.BackgroundColor	= 'red';
-buttonHandle.FontSize           = 16;
-buttonHandle.FontWeight         = 'bold';
+    buttonHandle = uicontrol(figureHandle);
+    set(buttonHandle,'Style','pushbutton');
+    set(buttonHandle,'String','STOP');
+    set(buttonHandle,'Position',[20 20 100 40]);
+    set(buttonHandle,'Callback','runflag=false;');
+    set(buttonHandle,'BackgroundColor','red');
+    set(buttonHandle,'FontSize',16);
+    set(buttonHandle,'FontWeight','bold');
 
-axisHandle	= gca;
-axisUnits	= axisHandle.Units;
-buttonUnits = buttonHandle.Units;
+    axisHandle	= gca;
+    axisUnits	= get(axisHandle,'Units');
+    buttonUnits = get(buttonHandle,'Units');
 
-axisHandle.Units	= 'pixels';
-buttonHandle.Units	= 'pixels';
+    set(axisHandle,'Units','pixels');
+    set(buttonHandle,'Units','pixels');
 
-% Reposition the button
-figurePosition	= figureHandle.Position;
-axisPosition	= axisHandle.Position;
-buttonPosition	= buttonHandle.Position;
+    % Reposition the button
+    figurePosition	= get(figureHandle,'Position');
+    axisPosition	= get(axisHandle,'Position');
+    buttonPosition	= get(buttonHandle,'Position');
 
-x = (axisPosition(1) - buttonPosition(3))/2;
-y = figurePosition(4) - x - buttonPosition(4);
-buttonPosition(1) = max([x,0]);
-buttonPosition(2) = max([y,0]);
-buttonHandle.Position = buttonPosition;
+    x = (axisPosition(1) - buttonPosition(3))/2;
+    y = figurePosition(4) - x - buttonPosition(4);
+    buttonPosition(1) = max([x,0]);
+    buttonPosition(2) = max([y,0]);
+    set(buttonHandle,'Position',buttonPosition);
 
-% Return units to starting value
-figureHandle.Units	= figureUnits;
-axisHandle.Units	= axisUnits;
-buttonHandle.Units  = buttonUnits;
+    % Return units to starting value
+    set(figureHandle,'Units',figureUnits);
+    set(axisHandle,'Units',axisUnits);
+    set(buttonHandle,'Units',buttonUnits);
+else % Code for newer MATLAB
+    figureUnits = figureHandle.Units;
+    figureHandle.Units = 'pixels';
+
+    buttonHandle = uicontrol(figureHandle);
+    buttonHandle.Style              = 'pushbutton';
+    buttonHandle.String             = 'STOP';
+    buttonHandle.Position           = [20 20 100 40];
+    buttonHandle.Callback           = 'runflag=false;';
+    buttonHandle.BackgroundColor	= 'red';
+    buttonHandle.FontSize           = 16;
+    buttonHandle.FontWeight         = 'bold';
+
+    axisHandle	= gca;
+    axisUnits	= axisHandle.Units;
+    buttonUnits = buttonHandle.Units;
+
+    axisHandle.Units	= 'pixels';
+    buttonHandle.Units	= 'pixels';
+
+    % Reposition the button
+    figurePosition	= figureHandle.Position;
+    axisPosition	= axisHandle.Position;
+    buttonPosition	= buttonHandle.Position;
+
+    x = (axisPosition(1) - buttonPosition(3))/2;
+    y = figurePosition(4) - x - buttonPosition(4);
+    buttonPosition(1) = max([x,0]);
+    buttonPosition(2) = max([y,0]);
+    buttonHandle.Position = buttonPosition;
+
+    % Return units to starting value
+    figureHandle.Units	= figureUnits;
+    axisHandle.Units	= axisUnits;
+    buttonHandle.Units  = buttonUnits;
+end
 
 end
 
