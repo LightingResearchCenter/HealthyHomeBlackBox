1ee86f89ab025d3b13284c5867d827d17081adc4
Geoffrey Jones <jonesg5@rpi.edu>
2015-06-03 16:18:21 -0400

    Nested rk4Sec into rk4stepperSec

--- rk4stepperSec.m
+++ rk4stepperSec.m
@@ -4,7 +4,7 @@ function [tf,xf,xcf] = rk4stepperSec(x0,xc0,lightDrive,ti,tf,nsteps,LRCparam)
 
 % Create local variables
 Bdrive = 0.56 * lightDrive;
-deltat = (tf - ti)/nsteps;
+hInterval = (tf - ti)/nsteps;
 
 % Initialize loop variables
 t = ti;
@@ -12,15 +12,48 @@ t = ti;
 % Loop
 for iStep = 1:nsteps
     % Calculate values per step
-    [x0, xc0] = rk4Sec(x0,xc0,Bdrive,deltat,LRCparam);
+    [x0, xc0] = rk4Sec(x0,xc0);
     
     % Update loop variable
-    t = t + deltat;
+    t = t + hInterval;
 end
 
 % Create return values
 tf = t;
 xf = x0;
 xcf = xc0;
+
+    function [xout,xcout] = rk4Sec(x0,xc0)
+        % RK4SEC calculates the derivatives of the pacemaker model at each step
+        
+        % Calculate derivatives
+        [xprime1,xcprime1] = xprimeSec(x0,xc0);
+        x1  = hInterval/2*xprime1  + x0;
+        xc1 = hInterval/2*xcprime1 + xc0;
+        
+        [xprime2,xcprime2] = xprimeSec(x1,xc1);
+        x2  = hInterval/2*xprime2  + x0;
+        xc2 = hInterval/2*xcprime2 + xc0;
+        
+        [xprime3,xcprime3] = xprimeSec(x2,xc2);
+        x3  = hInterval*xprime3  + x0;
+        xc3 = hInterval*xcprime3 + xc0;
+        
+        xdym  = 2*(xprime3  + xprime2);
+        xcdym = 2*(xcprime3 + xcprime2);
+        
+        [xprime4,xcprime4] = xprimeSec(x3,xc3);
+        
+        % Create output valriables
+        xout  = x0  + hInterval/6*(xprime1  + xprime4  + xdym);
+        xcout = xc0 + hInterval/6*(xcprime1 + xcprime4 + xcdym);
+
+        function [xprime,xcprime] = xprimeSec(x,xc)
+            % Model
+            xprime  = pi/43200*(LRCparam.mu*(x/3 + 4/3*x^3 - 256/105*x^7) + xc + Bdrive);
+            xcprime = pi/43200*(LRCparam.q*Bdrive*xc - (24/(0.99729*LRCparam.tau))^2*x + LRCparam.k*Bdrive*x);
+        end
+    end
+
 
 end
