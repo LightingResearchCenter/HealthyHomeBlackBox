28c57ab910e3a043c28b9c0611c39a11e325c5e7
Geoffrey Jones <jonesg5@rpi.edu>
2015-06-03 15:30:13 -0400

    modified rk4Sec to use for loop

--- rk4Sec.m
+++ rk4Sec.m
@@ -1,23 +1,27 @@
-function [xout,xcout] = rk4Sec(x0,xc0,Bdrive,hInterval)
+function [xout,xcout] = rk4Sec(x0,xc0,Bdrive,hInterval,LRCparam)
 % RK4SEC calculates the derivatives of the pacemaker model at each step
 
-%% Calculate derivatives
-[xprime1,xcprime1] = xprimeSec(x0,xc0,Bdrive);
-x1 = hInterval/2*xprime1+x0;
-xc1 = hInterval/2*xcprime1+xc0;
-[xprime2,xcprime2] = xprimeSec(x1,xc1,Bdrive);
-x2 = hInterval/2*xprime2+x0;
-xc2 = hInterval/2*xcprime2+xc0;
-[xprime3,xcprime3] = xprimeSec(x2,xc2,Bdrive);
-x3 = hInterval*xprime3+x0;
-xc3 = hInterval*xcprime3+xc0;
-xdym = 2*(xprime3+xprime2);
-xcdym = 2*(xcprime3+xcprime2);
-[xprime4,xcprime4] = xprimeSec(x3,xc3,Bdrive);
+% Preallocate variables
+n = 5;
+xprime  = zeros(n,1);
+xcprime = zeros(n,1);
+x  = zeros(n,1);
+xc = zeros(n,1);
+x(1)  = x0;
+xc(1) = xc0;
 
+% Calculate derivatives
+for i1 = 2:n
+    [xprime(i1),xcprime(i1)] = xprimeSec(x(i1-1),xc(i1-1),Bdrive,LRCparam);
+    x(i1) = hInterval/2*xprime(i1) + x0;
+    xc(i1) = hInterval/2*xcprime(i1) + xc0;
+end
 
-%% Create output valriables
-xout = x0+hInterval/6*(xprime1+xprime4+xdym);
-xcout = xc0+hInterval/6*(xcprime1+xcprime4+xcdym);
+xdym  = 2*(xprime(4)  + xprime(3));
+xcdym = 2*(xcprime(4) + xcprime(3));
+
+% Create output valriables
+xout  = x0  + hInterval/6*(xprime(2)  + xprime(5)  + xdym);
+xcout = xc0 + hInterval/6*(xcprime(2) + xcprime(5) + xcdym);
 
 end
