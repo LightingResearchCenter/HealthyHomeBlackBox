840b12ee51d96b6d0778a292df5ab8a814ad2fc1
Geoffrey Jones <jonesg5@rpi.edu>
2015-06-18 16:35:48 -0400

    Created LRCgapFill

---
+++ LRCgapFill.m
@@ -0,0 +1,32 @@
+function [newTimeUTC,newCs] = LRCgapFill(timeUTC,cs,expectedInc)
+%LRCGAPFILL Resample data to evenly spaced increments filling gaps with 0s.
+%   expectedInc must be in the same units as timeUTC
+%   timeUTC and cs must be vertical vectors with at least 2 entries
+
+% Find the time between samples
+sampleDiff = round(diff(timeUTC)/expectedInc); % Multiples of increment
+
+% Find large gaps between samples
+gap = sampleDiff > 1; % Effectively true if sampleDiff is 2 or greater
+
+gapStart = timeUTC([gap;false]); % Start time of each gap
+gapEnd = timeUTC([false;gap]); % End time of each gap
+nGap = numel(gapStart); % Number of gaps
+
+% Create evenly spaced time array
+newTimeUTC = (timeUTC(1):expectedInc:timeUTC(end))';
+% Resample the data to evenly spaced increments
+newCs = interp1(timeUTC,cs,newTimeUTC,'linear');
+
+% If there were large gaps replace the interpolated values with 0
+if nGap > 0
+    for iGap = 1:nGap
+        thisStart = gapStart(iGap);
+        thisEnd = gapEnd(iGap);
+        thisIdx = newTimeUTC > thisStart & newTimeUTC < thisEnd;
+        newCs(thisIdx) = 0;
+    end
+end
+
+end
+
